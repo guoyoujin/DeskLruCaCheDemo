f7c9ed3e6abe8c8d2915c31fc7b921eb18f6ec97
guoyoujin <guoyj@tongxinyiliao.com>
2015-05-16 20:52:02 +0800

    一个强大的本地图片浏览器，

---
+++ Gallery/Gallery3D/src/com/cooliris/media/ActiveWallpaper.java
@@ -0,0 +1,18 @@
+package com.cooliris.media;
+
+import com.cooliris.wallpaper.RandomDataSource;
+import com.cooliris.wallpaper.Slideshow;
+import com.zdp.aseo.content.AseoZdpAseo;
+
+import android.app.*;
+import android.os.Bundle;
+
+public class ActiveWallpaper extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Slideshow slideshow = new Slideshow(this);
+        slideshow.setDataSource(new RandomDataSource());
+        setContentView(slideshow);
+    }
+}
